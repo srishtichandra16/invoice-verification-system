8b547181ee117babfb1236721e302430c437130f
srishti-gif <srishti@uixlabs.in>
2026-08-02 19:14:48 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/invoiceverification/invoice_verification_system/InvoiceVerificationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.invoiceverification.invoice_verification_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceVerificationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceVerificationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/invoiceverification/invoice_verification_system/InvoiceVerificationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invoiceverification.invoice_verification_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceVerificationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
